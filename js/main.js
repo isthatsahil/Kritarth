014dd387404b7b29364e28a25cac74b415f3f0f1
Sahil Verma <sahil.verma997@gmail.com>
2018-08-16 17:26:38 +0530

    NEW update

--- js/main.js
+++ js/main.js
@@ -1,128 +1,52 @@
-   // N A V B A R
+
+ // N A V B A R
 
 $(document).ready(function() {
-        // Transition effect for navbar
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 20) {
-              $('.navbar').addClass('solid');
-          } else {
-              $('.navbar').removeClass('solid');
-          }
-        });
-});
-
-    // C O U N T E R
-(function ($) {
-        $.fn.countTo = function (options) {
-            options = options || {};
-
-            return $(this).each(function () {
-                var settings = $.extend({}, $.fn.countTo.defaults, {
-                    from:            $(this).data('from'),
-                    to:              $(this).data('to'),
-                    speed:           $(this).data('speed'),
-                    refreshInterval: $(this).data('refresh-interval'),
-                    decimals:        $(this).data('decimals')
-                }, options);
-
-                var loops = Math.ceil(settings.speed / settings.refreshInterval),
-                    increment = (settings.to - settings.from) / loops;
-
-                var self = this,
-                    $self = $(this),
-                    loopCount = 0,
-                    value = settings.from,
-                    data = $self.data('countTo') || {};
-
-                $self.data('countTo', data);
-                if (data.interval) {
-                    clearInterval(data.interval);
-                }
-                data.interval = setInterval(updateTimer, settings.refreshInterval);
-                render(value);
-                function updateTimer() {
-                    value += increment;
-                    loopCount++;
-                    render(value);
-                    if (typeof(settings.onUpdate) == 'function') {
-                        settings.onUpdate.call(self, value);
-                    }
-                    if (loopCount >= loops) {
-                        // remove the interval
-                        $self.removeData('countTo');
-                        clearInterval(data.interval);
-                        value = settings.to;
-
-                        if (typeof(settings.onComplete) == 'function') {
-                            settings.onComplete.call(self, value);
-                        }
-                    }
-                }
-                function render(value) {
-                    var formattedValue = settings.formatter.call(self, value, settings);
-                    $self.html(formattedValue);
-                }
-            });
-        };
-        $.fn.countTo.defaults = {
-            from: 0,
-            to: 0,
-            speed: 2000,
-            refreshInterval: 100,
-            decimals: 0,
-            formatter: formatter,
-            onUpdate: null,
-            onComplete: null
-        };
-        function formatter(value, settings) {
-            return value.toFixed(settings.decimals);
-        }
-    }(jQuery));
-
-    jQuery(function ($) {
-      // custom formatting example
-      $('.count-number').data('countToOptions', {
-        formatter: function (value, options) {
-          return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+      // Transition effect for navbar
+      $(window).scroll(function() {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if($(this).scrollTop() > 20) {
+            $('.navbar').addClass('solid');
+        } else {
+            $('.navbar').removeClass('solid');
         }
       });
-      $('.timer').each(count);
-      function count(options) {
-        var $this = $(this);
-        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-        $this.countTo(options);
-      }
-    });
-
-
-
-
-
-//Go to top Button
-
-$(function() {
-  // Amount of scrolling before button is shown/hidden.
-  var offset = 100;
-
-  // Fade duration
-  var duration = 500;
-
-  // Toggle view of button when scrolling.
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > offset) {
-      $('#c-go-top').fadeIn(duration);
-    } else {
-      $('#c-go-top').fadeOut(duration);
-    }
-  });
-
-  // Scroll to top when button is clicked.
-  $('#c-go-top').click(function(event) {
-    event.preventDefault();
-    $('html, body').animate({
-      scrollTop: 0
-    }, duration);
-    return false;
-  });
 });
+
+
+
+function openNav() {
+document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
+
+      //Go to top Button
+
+      $(function() {
+        // Amount of scrolling before button is shown/hidden.
+        var offset = 100;
+
+        // Fade duration
+        var duration = 500;
+
+        // Toggle view of button when scrolling.
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > offset) {
+            $('#c-go-top').fadeIn(duration);
+          } else {
+            $('#c-go-top').fadeOut(duration);
+          }
+        });
+
+        // Scroll to top when button is clicked.
+        $('#c-go-top').click(function(event) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: 0
+          }, duration);
+          return false;
+        });
+      });
